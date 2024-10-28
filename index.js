165010dd91c2789813e79910092d973d8a1275d5
NoahGagne <noahgagn4@gmail.com>
2024-10-28 10:01:20 -0400

    réparer affichageTexte 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {BigNumber} from '/bignumber.js-9.1.1/bignumber.mjs';
 
-let affichageTexte = document.getElementById('affichageTexte').innerText;
+let affichageTexte = document.getElementById('affichageTexte');
 const bouton1 = document.getElementById('un');
 const bouton2 = document.getElementById('deux');
 const bouton3 = document.getElementById('trois');
